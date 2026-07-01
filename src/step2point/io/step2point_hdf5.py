1186ae706a040d564fcf6f90b079e026331c188b
siyuChen-gif <siyuche4@andrew.cmu.edu>
2026-07-01 18:43:15 +0800

    fix subdetector

--- src/step2point/io/step2point_hdf5.py
+++ src/step2point/io/step2point_hdf5.py
@@ -42,7 +42,10 @@ class Step2PointHDF5Reader(ShowerReader):
                         "momentum": tuple(map(float, p_mom[i])),
                     }
             step = h5["steps"]
-            subdetector = np.asarray(steps["subdetector"], dtype=np.uint8)
+            if "subdetector" in steps:
+                subdetector = np.asarray(steps["subdetector"], dtype=np.uint8)
+            else:
+                subdetector = np.zeros(len(steps["energy"]), dtype=np.uint8)
             subdetector_names = [
                 x.decode("utf-8") if isinstance(x, bytes) else str(x)
                 for x in h5["metadata"]["subdetector_names"][:]
