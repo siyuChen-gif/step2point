ea19efa0189ca08fc6928b0f89b3a013e22d025b
siyuChen-gif <siyuche4@andrew.cmu.edu>
2026-07-03 17:54:41 +0800

    debug

--- src/step2point/algorithms/hdbscan_clustering.py
+++ src/step2point/algorithms/hdbscan_clustering.py
@@ -174,8 +174,7 @@ class HDBSCANClustering(CompressionAlgorithm):
         subdetectors = np.asarray(subdetectors, dtype=np.int64)
         decoded = np.empty(cell_ids.shape[0], dtype=np.int64)
         unique_subdetectors = np.unique(subdetectors)
-        subdetector_names = shower.metadata.get("subdetector_names")
-        print(subdetector_names, type(subdetector_names))
+        subdetector_names = shower.metadata.get("subdetector_names", [])
         MAP = {isub: name for isub, name in enumerate(subdetector_names)}
         # checks
         if self.collection_name:
@@ -218,7 +217,7 @@ class HDBSCANClustering(CompressionAlgorithm):
         subdetectors = np.asarray(subdetectors, dtype=np.int64)
         decoded = np.empty(cell_ids.shape[0], dtype=np.int64)
         unique_subdetectors = np.unique(subdetectors)
-        subdetector_names = shower.metadata.get("subdetector_names")
+        subdetector_names = shower.metadata.get("subdetector_names", [])
         MAP = {isub: name for isub, name in enumerate(subdetector_names)}
         # checks
         if self.collection_name:
